64b537d03de8bcfe54c3ad52bdd3a99b97727184
pavel <pugachenkopavel72@gmail.com>
2025-06-15 23:25:51 +0500

    bug fixes

--- appointment/test_models.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.urls import reverse
-from .models import Appointment
-
-@pytest.mark.django_db
-def test_model_creation():
-    """Проверка создания модели"""
-    obj = Appointment.objects.create(name="Test Object")
-    assert obj.name == "Test Object"
